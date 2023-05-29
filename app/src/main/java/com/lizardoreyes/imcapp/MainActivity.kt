7baad3d2cccc79d2ffdec585ca728b108deb10fc
Lizardo Reyes Jara <ciberutilidades@gmail.com>
2023-05-28 20:38:30 -0500

    Finishing main activity

--- app/src/main/java/com/lizardoreyes/imcapp/MainActivity.kt
+++ app/src/main/java/com/lizardoreyes/imcapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.lizardoreyes.imcapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -9,6 +10,11 @@ import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+    private var currentWeight: Int = 62
+    private var currentHeight: Float = 120f
+    private var currentAge : Int = 26
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +24,50 @@ class MainActivity : AppCompatActivity() {
         initListeners()
     }
 
-    private fun initListeners() {
-        binding.cardFemale.setOnClickListener { setGenderColor(Gender.FEMALE) }
-        binding.cardMale.setOnClickListener { setGenderColor(Gender.MALE) }
-        binding.rsHeight.addOnChangeListener { _, value, _ ->
-            val df = DecimalFormat("#.##")
-            val result = df.format(value)
-            binding.tvHeight.text = getString(R.string.result_height, result)
+    private fun initListeners() = with(binding) {
+        cardFemale.setOnClickListener { setGenderColor(Gender.FEMALE) }
+        cardMale.setOnClickListener { setGenderColor(Gender.MALE) }
+        rsHeight.addOnChangeListener { _, value, _ -> listenerOnChangeHeight(value) }
+        btnAddWeight.setOnClickListener { updateCurrentWeight(1) }
+        btnSubtractWeight.setOnClickListener { updateCurrentWeight(-1) }
+        btnAddAge.setOnClickListener { updateCurrentAge(1) }
+        btnSubtractAge.setOnClickListener { updateCurrentAge(-1) }
+        btnCalculate.setOnClickListener { calculateIMC() }
+    }
+
+    private fun calculateIMC() {
+        val imc = currentWeight / (currentHeight * currentHeight) * 10000
+        val imcResult = when {
+            imc < 18.5 -> getString(R.string.imc_underweight)
+            imc < 24.9 -> getString(R.string.imc_normal)
+            imc < 29.9 -> getString(R.string.imc_overweight)
+            else -> getString(R.string.imc_obesity)
         }
+
+        Toast.makeText(this, getString(R.string.result_imc, imc, imcResult), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateCurrentAge(change: Int) {
+        if (change > 0)
+            currentAge += change
+        else if (currentAge in 1..120)
+            currentAge += change
+        binding.tvAge.text = currentAge.toString()
+    }
+
+    private fun updateCurrentWeight(change: Int) {
+        if (change > 0)
+            currentWeight += change
+        else if (currentWeight in 1..300)
+            currentWeight += change
+        binding.tvWeight.text = currentWeight.toString()
+    }
+
+    private fun listenerOnChangeHeight(value: Float) = with(binding) {
+        currentHeight = value
+        val df = DecimalFormat("#.##")
+        val result = df.format(currentHeight)
+        tvHeight.text = getString(R.string.result_height, result)
     }
 
     private fun setGenderColor(gender: Gender) = with(binding) {
@@ -36,10 +78,14 @@ class MainActivity : AppCompatActivity() {
             Gender.MALE -> {
                 cardMaleBackground = R.color.colorPrimaryDark
                 cardFemaleBackground = R.color.colorPrimary
+                isMaleSelected = true
+                isFemaleSelected = false
             }
             Gender.FEMALE -> {
                 cardFemaleBackground = R.color.colorPrimaryDark
                 cardMaleBackground = R.color.colorPrimary
+                isMaleSelected = false
+                isFemaleSelected = true
             }
         }
 
